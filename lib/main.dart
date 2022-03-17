45234b91c2e5ab6d72508c7b89d2f1edf3fd628a
ashrafffathalla <75317437+ashrafffathalla@users.noreply.github.com>
2022-03-17 02:10:14 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:login/screens/welcome_screen.dart';
+import 'package:login/shared/colors.dart'as colors;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: colors.kPrimaryColor,
+        appBarTheme: const AppBarTheme(
+          backwardsCompatibility: false,
+          elevation: 0,
+           systemOverlayStyle: SystemUiOverlayStyle(
+               statusBarColor: Colors.transparent,
+               statusBarIconBrightness: Brightness.dark,
+           ),
+        ),
+      ),
+
+      home: const WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/shared/components/body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
